6457a6bf837fb9de6fb34b0676fe5bfae1e8a316
Yura-student <yuriy.storozhev@mail.ru>
2023-06-18 22:45:32 +0700

    Добавлена шапка

---
+++ src/app/resource/resource-page/resource-page.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { Router } from '@angular/router';
+
+@Component({
+  selector: 'app-resource-page',
+  templateUrl: './resource-page.component.html',
+  styleUrls: ['./resource-page.component.scss'],
+})
+export class ResourcePageComponent {
+  constructor(private fireAuth: AngularFireAuth, private router: Router) {}
+  //выход
+  logout() {
+    this.fireAuth.signOut().then(() => {
+      this.router.navigate(['/']);
+    });
+  }
+
+  add() {
+    this.router.navigate(['/add']);
+  }
+}
